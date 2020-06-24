1191bda9337b23ad0a4997154e2a75e656f61f03
Lev Maximov <lev.maximov@gmail.com>
2020-06-24 22:15:29 +0700

    admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 
+from .models import User
+
+@register(User)
+class CustomUserAdmin(UserAdmin):
+    list_filter = []
+    list_display = ['email']
+    exclude = []
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': ('active', 'staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    ordering = 'email',
 # Register your models here.
